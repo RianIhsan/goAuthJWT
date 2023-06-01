f311569a0610840e713ff31a702d80f4bd553dd5
Reon is Rian <93025581+RianIhsan@users.noreply.github.com>
2023-06-01 22:21:39 +0700

    Update main.go

--- main.go
+++ main.go
@@ -22,9 +22,10 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5173",
-		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
-		AllowCredentials: true,
+		AllowOrigins:     "*",
+		AllowHeaders:     "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+		AllowMethods:     "GET, POST, PUT, DELETE, OPTIONS",
+		AllowCredentials: false,
 	}))
 
 	routes.InitRoute(app)
